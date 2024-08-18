939a659ef5c500b67c48d6656726c4c605bd9a6e
lucaslangsch <lucas.langsch@gmail.com>
2024-08-18 08:09:27 -0300

    styles: navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,26 @@
 
 import { useContext } from 'react';
 import AuthContext from '../../context/AuthContext'
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { common } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const { name } = useContext(AuthContext);
 
   return (
-    <AppBar sx={{ position: 'fixed', backgroundColor: '#a32424' }}>
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    <AppBar color='primary'>
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Typography variant="h5">
           Olá, {name}
         </Typography>
+        <Link to={"/dashboard"}>
+        <IconButton size='large' sx={{ color: common.white }}>
+          <Typography variant="h5">Perfil&nbsp;</Typography>
+          <AccountCircleIcon />
+        </IconButton>
+        </Link>
       </Toolbar>
     </AppBar>
   );
