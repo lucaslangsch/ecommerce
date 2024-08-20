cac6841e8765116df9ae6ad61083c07aaf6f7f72
lucaslangsch <lucas.langsch@gmail.com>
2024-08-19 21:28:30 -0300

    feat: protect route

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,15 +1,26 @@
 import { Navigate, Outlet } from "react-router-dom";
 import AuthContext from '../context/AuthContext';
 import { useContext } from "react";
+import Navbar from "../components/navbar";
+import { Box } from "@mui/material";
 
 const RotaPrivada = () => {
   const { authenticated, loading } = useContext(AuthContext);
   if (loading) {
     return <div>Loading...</div>;
   }
-    return (
-      authenticated ? <Outlet /> : <Navigate to="/" />
+  return (
+    authenticated ? (
+      <>
+        <Navbar />
+        <Box sx={{ padding: 4, marginTop: '64px' }} component="main">
+          <Outlet />
+        </Box>
+      </>
+    ) : (
+      <Navigate to="/" />
     )
+  );
 }
 
 export default RotaPrivada;
