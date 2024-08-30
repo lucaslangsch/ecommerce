9a1cfae1d4384575626c3289ed56a609aad76c48
lucaslangsch <lucas.langsch@gmail.com>
2024-08-30 13:04:16 -0300

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/home';
 import Register from './pages/register';
 import RotaPrivada from './routes/PrivateRoute';
 import Dashboard from './pages/dashboard';
+import Plan from './pages/plan';
 import Plans from './pages/plans';
 
 function App() {
@@ -12,9 +13,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />}  />
       <Route element={<RotaPrivada />}>
-        <Route path="/dashboard" element={<Dashboard />} ></Route>
-        <Route path="/dashboard/plan" element={<Plans />} />
-      {/* </Route> */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/plans" element={<Plans />}/>
+        <Route path="/dashboard/plans/:id" element={<Plan />} />
       </Route>
     </Routes>
   )
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,28 +1,71 @@
-
-import { useContext } from 'react';
-import AuthContext from '../../context/AuthContext'
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { useState } from 'react';
+import { AppBar, IconButton, Toolbar, Typography, Drawer, List, ListItem, ListItemText, Box, ListItemButton, ListItemIcon } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { common } from '@mui/material/colors';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../../context/AuthContext';
 
 function Navbar() {
   const { name } = useContext(AuthContext);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+      return;
+    }
+    setIsDrawerOpen(open);
+  };
 
   return (
-    <AppBar color='primary'>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Typography variant="h5">
-          Olá, {name}
-        </Typography>
-        <Link to={"/dashboard"}>
-        <IconButton size='large' sx={{ color: common.white }}>
-          <Typography variant="h5">Perfil&nbsp;</Typography>
-          <AccountCircleIcon />
-        </IconButton>
-        </Link>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar color='primary'>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <IconButton edge="start" color="inherit" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h5">
+              Olá, {name}
+            </Typography>
+
+          </Box>
+          <Link to={"/dashboard"}>
+            <IconButton size='large' sx={{ color: common.white }}>
+              <Typography variant="h5">Perfil&nbsp;</Typography>
+              <AccountCircleIcon />
+            </IconButton>
+          </Link>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer(false)}>
+        <List>
+          <Link to="/dashboard" onClick={toggleDrawer(false)}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Perfil" />
+            </ListItemButton>
+          </ListItem>
+          </Link>
+          <Link to="/dashboard/plans" onClick={toggleDrawer(false)}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <ShoppingCartIcon />
+              </ListItemIcon>
+              <ListItemText primary="Planos" />
+            </ListItemButton>
+          </ListItem>
+          </Link>
+        </List>
+      </Drawer>
+    </>
   );
 }
 
